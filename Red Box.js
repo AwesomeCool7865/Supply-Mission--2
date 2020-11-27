ebd3cafed23dc6a289cb7574baccccfb996990cc
AwesomeCool7865 <73596567+AwesomeCool7865@users.noreply.github.com>
2020-11-27 16:46:57 +0530

    Add files via upload

---
+++ Red Box.js
@@ -0,0 +1,22 @@
+class RedBox {
+    constructor(x, y, width, height) {
+      var options = {
+          'friction':1.0,
+          'density':1.0
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      this.body.shapeColor= 'red'
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      rotate(angle);
+      rectMode(CENTER);
+      rect(0, 0, this.width, this.height);
+    }
+  };
+  
